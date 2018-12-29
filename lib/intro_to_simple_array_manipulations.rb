659b321006f774fef5a4c9285544f9a6a5c3191e
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-29 00:37:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,6 @@ def using_push(countries_in_western_africa, next_country)
 end   
 
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood  )
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
